f394f84e36ec2af2922e80e4c7efeff844f62585
naoki-nagazumi <naoki.nagazumi@ntt-at.co.jp>
2017-09-19 19:24:51 +0900

    just try vue.js, vue component

---
+++ vue-tutorial-02/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<div id="app">
+  <myComponent v-bind:message="message"
+    v-bind:custom-message="customMessage"
+    v-bind:on-load="onChildLoaded"
+    v-on:upVal="updateValue"
+  />
+</div>
+</template>
+
+<script>
+import myComponent from './MyComponent.vue';
+
+export default {
+  data: function () {
+    return {
+      message: 'Hello World',
+    };
+  },
+  computed: {
+    customMessage: function() {
+      return `This is Custom from ${this.message}`;
+    },
+  },
+  methods: {
+    updateValue: function(value) {
+      this.message = value;
+    },
+    onChildLoaded: function (msg) {
+      console.log(msg);
+    },
+  },
+  components: { myComponent },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-tutorial-02/src/MyComponent.vue
@@ -0,0 +1,29 @@
+<template>
+<section>
+  <h2 class="my-app">my template</h2>
+    <p>message : {{ message }}</p>
+    <p>custom : {{ customMessage }}</p>
+    <p><input :value="message" @input="upmes($event.target.value)"></p>
+</section>
+</template>
+
+<script>
+export default {
+  props: ['message', 'customMessage', 'onLoad'],
+  mounted: function () {
+    this.onLoad('message from child!');
+  },
+  methods: {
+    upmes: function(value) {
+      this.onLoad(`value = ${value}`);
+      this.$emit('upVal', value);
+    },
+  },
+};
+</script>
+
+<style>
+h2.my-app {
+  color : blue;
+}
+</style>
